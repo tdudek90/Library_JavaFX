b9c881cb3c4e4ddea5062d1835dc01ab6be4f4b4
Tomek Dudek <ttomekk0@gmail.com>
2017-07-06 19:03:24 +0200

    added registration method

--- src/Controller.java
+++ src/Controller.java
@@ -3,7 +3,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,12 +16,24 @@ public class Controller implements Initializable {
 
     @FXML
     TextField loginText;
-
     @FXML
     PasswordField passwordText;
+    @FXML
+    TextField name;
+    @FXML
+    TextField lastName;
+    @FXML
+    PasswordField passwordRegText;
+    @FXML
+    PasswordField rePasswordText;
+    @FXML
+    TextField number;
 
 
     public void signIn() {
+        if (isFormLoginValid()) {
+            return;
+        }
 
         System.out.println(loginText.getText() + " " + passwordText.getText());
         Statement statement = ServerConnection.getInstance().getNewStatement();
@@ -54,4 +68,61 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+
+    private boolean isFormLoginValid() {
+        if (loginText.getText().trim().length() > 4 || passwordText.getText().trim().length() > 4) {
+            return true;
+        }
+        Utils.openAlert("Sign in", "Your login and password must be at least 4 characters long");
+        return false;
+    }
+
+    public void register()  {
+        try {
+            if (isLoginExist()){
+                Utils.openAlert("Error", "Login already exist");
+                return;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ServerConnection serverConnection = ServerConnection.getInstance();
+        String sql = "INSERT INTO user (name, lastName, number, password) VALUES (?,?,?,?)";
+        PreparedStatement statement = serverConnection.getNewPrepareStatement(sql);
+        try {
+            statement.setString(1, name.getText());
+            statement.setString(2, lastName.getText());
+            statement.setString(3, passwordRegText.getText());
+            statement.setString(4, rePasswordText.getText());
+            statement.setString(4, number.getText());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            statement.execute();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("User has been registered!");
+    }
+
+    private boolean isLoginExist() throws SQLException {
+        ServerConnection serverConnection = ServerConnection.getInstance();
+        String imie = name.getText();
+        String sql = "SELECT * FROM user WHERE name = ?";
+        PreparedStatement statement = serverConnection.getNewPrepareStatement(sql);
+        statement.setString(1, imie);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            System.out.println("User exist");
+            return true;
+        } else {
+            System.out.println("User does not exist");
+            return false;
+        }
+    }
+
 }
